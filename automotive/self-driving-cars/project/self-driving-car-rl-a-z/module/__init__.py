3747615e68d0bbc42441d07fbe3db8a89ea93799
afondiel <studenetexchange@gmail.com>
2022-11-14 22:49:47 +0100

    my-lab-auto-self-driving-a-z: solve import issue btw submodules of modules dir

---
+++ automotive/self-driving-cars/project/self-driving-car-rl-a-z/module/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+if 'module' not in sys.path:
+    import os
+    module_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(module_path)
+    sys.path.append(module_path + "/brain")
+    sys.path.append(module_path + "/environment")

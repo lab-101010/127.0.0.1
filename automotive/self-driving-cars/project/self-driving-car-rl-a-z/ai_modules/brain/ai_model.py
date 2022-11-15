de02b4e7de297f09b4abf98a661f90dff75fe7ef
afondiel <studenetexchange@gmail.com>
2022-11-15 19:37:29 +0100

    my-lab-auto-self-driving-a-z: update Model documentation

--- automotive/self-driving-cars/project/self-driving-car-rl-a-z/ai_modules/brain/ai_model.py
+++ automotive/self-driving-cars/project/self-driving-car-rl-a-z/ai_modules/brain/ai_model.py
@@ -15,6 +15,9 @@
 #		- Provides data structures for multi-dimensional tensors and defines mathematical operations over these tensors
 #       - provides also many utilities for efficient serializing of Tensors and arbitrary types, and other useful utilities.
 #       - It has a CUDA counterpart, that enables you to run your tensor computations on an NVIDIA GPU with compute capability >= 3.0.
+#   <settings> : local module 
+#       - provides parameters calibrations for model tuning
+#       - provides config parameters and constant for the App
 
 # File history :
 # Afondiel  |  11.11.2022 | Creation 
@@ -29,6 +32,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+# Variable and Tensor already merged since the torch v0.4.x
+# No need to convert tensor to variable no more 
 import torch.autograd as autograd
 from torch.autograd import Variable
 
@@ -39,17 +44,23 @@ import ai_settings
 class Network(nn.Module):
     """ Deep Feed Forward (DFF) Neural Network """
     def __init__(self, input_size, hidden_size, nb_action):
+        """object constructor
+        - self: this object
+        - input_size : inputs layers (5 nodes : 3 sensor signals, +orientation, -orientation)
+        - hidden_size : hidden layer (30 nodes)
+        - nb_action : output layer (3 signals : left, straight, right )
+        """
         super(Network, self).__init__()
         self.input_size = input_size
         self.nb_action  = nb_action
-        self.fc1 = nn.Linear(input_size, hidden_size) #hidden layer=30
-        self.fc2 = nn.Linear(hidden_size, nb_action)
+        self.fc1        = nn.Linear(input_size, hidden_size)
+        self.fc2        = nn.Linear(hidden_size, nb_action)
         # activation function
-        self.relu = nn.ReLU()
+        self.activation = nn.ReLU()
     
     def forward(self, state):
         # x = F.relu(self.fc1(state))
         x        = self.fc1(state)
-        x        = self.relu(x)
-        q_values = self.fc2(x)
+        _x       = self.activation(x)
+        q_values = self.fc2(_x)
         return q_values

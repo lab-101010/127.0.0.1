9652af96c6b986b88a255101c65583e9211fa66c
afondiel <studenetexchange@gmail.com>
2022-11-29 01:07:25 +0100

    game-breakout-rl: solve import issue in the modules

---
+++ game/breakout-rl-a3c/modules/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+if 'modules' not in sys.path:
+    import os
+    module_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(module_path)
+    sys.path.append(module_path + "/brain")
+    sys.path.append(module_path + "/environment")
+    # sys.path.append(module_path + "/common")
